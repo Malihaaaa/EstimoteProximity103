7573d7a4fe4817d15a13f416935ce8a8f5659f71
Malihaaaa <78786670+Malihaaaa@users.noreply.github.com>
2022-04-06 16:24:33 +0200

    Initial commit

---
+++ app/src/main/java/dtu/engtech/estimoteproximity103/CloudCredentials.kt
@@ -0,0 +1,6 @@
+package dtu.engtech.estimoteproximity103
+
+object CloudCredentials {
+    const val APP_ID = "instantsos-98w"
+    const val APP_TOKEN = "086760656efc7843517021a780f8b3fc"
+}
---
+++ app/src/main/java/dtu/engtech/estimoteproximity103/DeviceList.kt
@@ -0,0 +1,11 @@
+package dtu.engtech.estimoteproximity103
+
+object DeviceList {
+    val deviceNames = mapOf("b25e839a7b9b5d8758e80dc225825939" to "504",
+        "507362792590e5b01740824c2df4c11c" to "505",
+        "b3a725ae7f7c7b26d2fe03be171b9b13" to "506")
+}
+
+
+
+
